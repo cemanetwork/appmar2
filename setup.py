41074734ff95100d202c17c3c08f4393c4b3e78c
dact221 <35376194+dact221@users.noreply.github.com>
2021-03-09 18:12:10 -0500

    Add scipy dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='appmar2',
     version='2.0.1',
     packages=find_packages(),
-    install_requires=['pygubu>=0.10.1', 'pandas>=1.0.3', 'numpy>=1.18.1', 'xarray>=0.15.1',
+    install_requires=['pygubu>=0.10.1', 'pandas>=1.0.3', 'numpy>=1.18.1', 'xarray>=0.15.1', 'scipy>=1.3.2',
                       'matplotlib>=3.1.3', 'seaborn>=0.10.0', 'statsmodels>=0.11.0', 'Cartopy>=0.17.0'],
     python_requires='>=3.7',
     include_package_data=True,
